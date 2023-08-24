21c5453ffab7b2722435fa2bae9f7a2be5ae71ab
rac101ran <rachit.ranjanapex23@gmail.com>
2023-08-24 06:07:34 +0530

    fragments Added

--- app/src/main/java/com/example/youtubeapp/MainActivity.kt
+++ app/src/main/java/com/example/youtubeapp/MainActivity.kt
@@ -3,12 +3,25 @@ package com.example.youtubeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import com.example.youtubeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportFragmentManager.beginTransaction().replace(R.id.frameLayoutId, AllVideosFragment()).commit()
+
+        binding.explorerText2.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.frameLayoutId, WatchLaterFragment()).commit()
+        }
+
+        binding.explorerText1.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.frameLayoutId, AllVideosFragment()).commit()
+        }
+
     }
 }
